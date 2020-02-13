fa634460cd34989c6975cafa1504be76b9c892e4
Abhimanyu <abhimanyu@localhost.localdomain>
2020-02-14 01:04:11 +0530

    more

--- Pattern_Magic.cpp
+++ Pattern_Magic.cpp
@@ -1,25 +1,60 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
-    cin>>t;
-    for(int row=1;row<=t;row++)
+    cin >> t;
+    for (int i = 1; i < 2 * t; i++)
     {
-        for(int col=0; col<2*t;col++)
+        cout << '*';
+    }
+    cout << endl;
+
+    for (int row = t - 1; row > 1; row--)
+    {
+        for (int col = 1; col < t + row; col++)
         {
-            if(col<row) cout<<'*';
-        }
-        for (int col = 2 * t; col >0; col--)
-        {
-            if (col < row)
+            if (col <= row)
                 cout << '*';
-            else
-                cout << ' ';
+        }
+        for (int col = 1; col < 2 * (t - row); col++)
+        {
+            cout << ' ';
+        }
+        for (int col = 1; col < t + row; col++)
+        {
+            if (col <= row)
+                cout << '*';
         }
         cout << endl;
     }
+
+    for (int row = 1; row <= t - 1; row++)
+    {
+        for (int col = 1; col < t + row; col++)
+        {
+            if (col <= row)
+                cout << '*';
+        }
+        for (int col = 1; col < 2 * (t - row); col++)
+        {
+            cout << ' ';
+        }
+        for (int col = 1; col < t + row; col++)
+        {
+            if (col <= row)
+                cout << '*';
+        }
+        cout << endl;
+    }
+
+    for (int i = 1; i < 2 * t; i++)
+    {
+        cout << '*';
+    }
+    cout << endl;
+
     return 0;
 }
