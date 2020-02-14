91c73f27fe09a910a67bd61ebca44ac20aa221b7
Abhimanyu <abhimanyu@localhost.localdomain>
2020-02-14 22:11:32 +0530

    add

--- Arrays-Sum_Of_Two_Arrays.cpp
+++ Arrays-Sum_Of_Two_Arrays.cpp
@@ -16,21 +16,47 @@ int main()
         cin >> B[i];
     }
     short res[max(n, m) + 2];
-    for (int i = min(n, m)-1,j=max(n,m)+1; i >=0; i--,j--)
+    for (int i = min(n, m) - 1, j = max(n, m) + 1; i >= 0; i--, j--)
     {
         tmp = A[i] + B[i] + carry;
-        res[j] = tmp%10;
-        carry = tmp/10;
+        res[j] = tmp % 10;
+        carry = tmp / 10;
     }
-    int i = max(m,n)+2 -min(m,n);
-    if(n>m)
+    int i = max(m, n) + 2 - min(m, n);
+    if (n > m)
     {
-        while (i > 0)
+        while (carry > 0)
         {
-            
+            res[i] = (A[i] + carry) % 10;
+            carry = (A[i] + carry) / 10;
+            i--;
+        }
+        while (i >= 0)
+        {
+            res[i] = A[i];
+            i--;
         }
     }
-
-    
+    else
+    {
+        while (carry > 0)
+        {
+            res[i] = (B[i] + carry) % 10;
+            carry = (B[i] + carry) / 10;
+            i--;
+        }
+        while (i >= 0)
+        {
+            res[i] = B[i];
+            i--;
+        }
+    }
+    bool flag=false;
+    for (int i = 0; i < max(n, m) + 2; i++)
+    {
+        if(A[i]>0 and flag==false) flag=true;
+        if(flag) cout<<A[i]<<", ";
+    }
+    cout<<"END";
     return 0;
 }
