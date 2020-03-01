15bb929ae5cc0ea455f44e8e49374963d9df97be
Abhimanyu Maurya <aerma7309@gmail.com>
2020-03-02 02:09:30 +0530

    add

--- Maximum_XOR.cpp
+++ Maximum_XOR.cpp
@@ -1,13 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int n,k;
-    cin>>n>>k;
- 
+    unsigned long long l, r, m, res = 0, i = 0, msk = 0;
+    cin >> l >> r;
+    m = l ^ r;
+    while (m > 0)
+    {
+        i++;
+        m = m >> 1;
+    }
 
+    while (i--)
+    {
+        res = (res << 1) | 1;
+    }
+    cout << res;
     return 0;
 }
