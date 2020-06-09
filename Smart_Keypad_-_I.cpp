367ac5fc1350758c6ab016d59e9093b0a065cf85
Abhimanyu Maurya <aerma7309@gmail.com>
2020-06-09 12:39:49 +0530

    Smart_Keypad_-_I.cpp

--- Smart_Keypad_-_I.cpp
+++ Smart_Keypad_-_I.cpp
@@ -8,12 +8,6 @@ bool ib = ios_base::sync_with_stdio(0);
 bool it = cin.tie(0);
 bool ot = cout.tie(0);
 
-// define ONLINE_JUDGE
-#ifndef ONLINE_JUDGE
-FILE *fr = freopen("input.txt", "r", stdin);
-FILE *fw = freopen("output.txt", "w", stdout);
-#endif
-
 void print(char s[], char o[], int p, char table[][5])
 {
     if (s[p] == '\0')
