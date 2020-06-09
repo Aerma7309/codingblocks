fb4c2154e17dddda07b36fc4d26b3d5e275f5e1e
Abhimanyu Maurya <aerma7309@gmail.com>
2020-06-09 16:46:04 +0530

    Unique_Permutations

--- Unique_Permutations.cpp
+++ Unique_Permutations.cpp
@@ -1,43 +1,45 @@
 /*
-* @Date    : 2020-04-14 20:05:09
+* @Date    : 2020-06-09 16:30:07
 * @Author  : Abhimanyu Kumar Maurya (aerma7309@gmail.com)
 */
 #include <bits/stdc++.h>
 using namespace std;
 
-set<vector<string>> p;
+bool ib = ios_base::sync_with_stdio(0);
+bool it = cin.tie(0);
 
-void permute(vector<string> &vs, int s, int e)
+vector<vector<int>> p;
+
+void permute(vector<int> vs, int s, int e)
 {
     if (s == e)
     {
-        p.insert(vs);
+        p.emplace_back(vs);
         return;
     }
     for (int j = s; j <= e; j++)
     {
+        if (s != j and vs[s] == vs[j])
+            continue;
         swap(vs[s], vs[j]);
         permute(vs, s + 1, e);
-        swap(vs[s], vs[j]);
     }
 }
 
-int main()
+signed main()
 {
     int n;
     cin >> n;
-    vector<string> vs(n);
-    for (int i = 0; i < n; i++)
-        cin >> vs[i];
-
-    permute(vs,0,n-1);
+    vector<int> vs(n);
+    for (auto &i : vs)
+        cin >> i;
+    sort(vs.begin(), vs.end());
+    permute(vs, 0, n - 1);
     for (auto &i : p)
     {
         for (auto &j : i)
-        {
-            cout<<j<<" ";
-        }
-        cout << "\n";
+            cout << j << ' ';
+        cout << '\n';
     }
     return 0;
 }
