e718f575a817e05b66d21a8ade2c21b8ba43e733
Abhimanyu Maurya <aerma7309@gmail.com>
2020-06-13 22:21:44 +0530

    Help_Rahul

--- Help_Rahul.cpp
+++ Help_Rahul.cpp
@@ -1,19 +1,29 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 int main()
 {
-    int n,key;
-    cin>>n;
-    vector<int> arr(n,0);
+    int n, key;
+    cin >> n;
+    vector<int> arr(n);
     for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cin >> key;
+    int i = 0, j = n - 1;
+    while (i <= j)
     {
-        cin>>arr[i];
+        int mid = (i + j) / 2;
+        if (arr[mid] == key)
+        {
+            cout << mid;
+            return 0;
+        }
+        else if (arr[mid] > arr[i] and key < arr[mid] and key >= arr[i])
+            j = mid - 1;
+        else //if(arr[mid] < arr[j] and key > arr[mid] and key <= arr[j])
+            i = mid + 1;
     }
-    cin>>key;
-    auto result = find(arr.cbegin(),arr.cend(),key) - arr.cbegin();
-    cout<<result;
-    
+
     return 0;
 }
